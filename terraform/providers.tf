21eda70eb131b3b18540e17a9b7380d9699b9ca1
Jesper Ryom <2834949+jryom@users.noreply.github.com>
2023-12-05 21:47:35 +0100

    init

---
+++ terraform/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.4.0"
+    }
+
+    elasticstack = {
+      source  = "elastic/elasticstack"
+      version = "0.8.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.2.0"
+    }
+  }
+}
